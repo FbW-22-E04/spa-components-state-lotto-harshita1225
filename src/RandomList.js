8e8094b6273ed3dc594494b15ee09cfe58c73af3
harshita <harshita1225@gmail.com>
2022-10-17 08:09:14 +0200

    completed

--- src/RandomList.js
+++ src/RandomList.js
@@ -1,61 +1,67 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-function RandomList() {
-  const [addNumber, setAddNumber] = useState([]);
-  let numberarray = [];
-  const generateRandomNum = () => {
-    const random0 = Math.ceil(Math.random() * 50);
-    const random1 = Math.ceil(Math.random() * 50);
-    const random2 = Math.ceil(Math.random() * 50);
-    const random3 = Math.ceil(Math.random() * 50);
-    const random4 = Math.ceil(Math.random() * 50);
-    const random5 = Math.ceil(Math.random() * 50);
-    const random6 = Math.ceil(Math.random() * 10);
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
 
-    // console.log(random);
-    numberarray = [
-      random0,
-      random1,
-      random2,
-      random3,
-      random4,
-      random5,
-      random6,
-    ];
-    // console.log(numberarray);
+function LuckyNumbers() {
+  const [numbers, setNumbers] = useState([]);
+  const [superNumber, setSuperNumber] = useState(null);
 
-    setAddNumber(numberarray);
+  const getLuckyNumbers = () => {
+    let numsCount = 6;
+    const nums = [];
+
+    // Get unique numbers
+    while (numsCount) {
+      let newNumer = getRandomIntInclusive(1, 49);
+
+      while (nums.indexOf(newNumer) !== -1) {
+        newNumer = getRandomIntInclusive(1, 49);
+      }
+
+      nums.push(newNumer);
+      numsCount--;
+    }
+
+    const supNum = getRandomIntInclusive(1, 10);
+    setNumbers(nums);
+    setSuperNumber(supNum);
+  };
+
+  const resetLuckyNumbers = () => {
+    setNumbers([]);
+    setSuperNumber(null);
   };
 
   return (
-    <>
-      <div className="flex gap-5">
-        {addNumber.map((item, idx) => (
-          <div className="flex my-5 items-center g-transparent  text-blue-700 font-semibold p-3  border border-blue-500 rounded-full">
-            <p className="w-[20px] h-[20px] text-center" key={idx}>
-              {item}
-            </p>
-          </div>
-        ))}
-      </div>
-      <div className="flex gap-[10px] my-5">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-3 rounded"
-          onClick={generateRandomNum}
-        >
-          Show me lucky numbers
-        </button>
-        <button
-          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-          onClick={() => {
-            setAddNumber([]);
-          }}
-        >
-          Reset
-        </button>
-      </div>
-    </>
+    <div>
+      <h1>Lotto 6 / 49</h1>
+      <p>Generating lucky numbers</p>
+
+      <p>
+        {numbers.length !== 0 &&
+          numbers.map((num) => {
+            return <span className="lucky-number">{num}</span>;
+          })}
+
+        {superNumber && (
+          <span className="lucky-number lucky-number--bonus">
+            {superNumber}
+          </span>
+        )}
+      </p>
+
+      <button onClick={resetLuckyNumbers} className="button button-reset">
+        Reset
+      </button>
+      <button type="reset" onClick={getLuckyNumbers} className="button">
+        Show me lucky numbers
+      </button>
+    </div>
   );
 }
 
-export default RandomList;
+export default LuckyNumbers;
